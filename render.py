2c670a33a8ae942fec82a82ff75a3e85e0576fbd
smbonilla <sierrabonilla14@gmail.com>
2023-11-30 15:14:31 +0000

    rotate about x y or z

--- render.py
+++ render.py
@@ -28,11 +28,13 @@ def render_set(model_path, name, iteration, views, gaussians, pipeline, backgrou
     makedirs(render_path, exist_ok=True)
     makedirs(gts_path, exist_ok=True)
 
+
     for idx, view in enumerate(tqdm(views, desc="Rendering progress")):
+        image_name = view.image_name
         rendering = render(view, gaussians, pipeline, background, kernel_size=kernel_size)["render"]
         gt = view.original_image[0:3, :, :]
-        torchvision.utils.save_image(rendering, os.path.join(render_path, '{0:05d}'.format(idx) + ".png"))
-        torchvision.utils.save_image(gt, os.path.join(gts_path, '{0:05d}'.format(idx) + ".png"))
+        torchvision.utils.save_image(rendering, os.path.join(render_path, image_name + ".png"))
+        torchvision.utils.save_image(gt, os.path.join(gts_path, image_name + ".png"))
 
 def render_sets(dataset : ModelParams, iteration : int, pipeline : PipelineParams, skip_train : bool, skip_test : bool):
     with torch.no_grad():
